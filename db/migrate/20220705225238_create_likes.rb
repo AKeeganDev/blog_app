926a74f8f0e72ec6142de73405489908c9185529
Aaron Keegan <AKeegan.Dev@gmail.com>
2022-07-06 11:37:46 -0400

    add migrations

---
+++ db/migrate/20220705225238_create_likes.rb
@@ -0,0 +1,11 @@
+class CreateLikes < ActiveRecord::Migration[5.2]
+  def change
+    create_table :likes do |t|
+      t.string :text
+      t.references :post, null:false, foreign_key: true
+      t.references :user, null:false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
