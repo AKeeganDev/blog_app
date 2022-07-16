047498f41fc5f3b86f32804e518ce25209335177
Aaron Keegan <AKeegan.Dev@gmail.com>
2022-07-15 20:35:48 -0400

    add comments

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,6 +2,8 @@ class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :post
 
+  validates :text, presence: true, length: { maximum: 4000 }
+
   after_save :increment_posts_comments_counter
 
   private
