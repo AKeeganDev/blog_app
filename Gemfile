702dacca4929b36a69bd368fec1f7c4c83a008e6
Aaron Keegan <AKeegan.Dev@gmail.com>
2022-07-20 15:34:58 -0400

    Devise setup

--- Gemfile
+++ Gemfile
@@ -49,6 +49,8 @@ gem 'sassc-rails'
 
 gem 'xmpp4r'
 
+gem 'devise'
+
 # Use Active Storage variants [https://guides.rubyonrails.org/active_storage_overview.html#transforming-images]
 # gem "image_processing", "~> 1.2"
 
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,25 @@
 class ApplicationController < ActionController::Base
-  def current_user
-    User.first
+  protect_from_forgery with: :exception
+
+  before_action :update_allowed_parameters, if: :devise_controller?
+
+  def print_current_user
+    p current_user
+  end
+
+  protected
+
+  def update_allowed_parameters
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :password) }
+    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name, :email, :password, :current_password) }
+  end
+
+  def authenticate_user!
+    if user_signed_in?
+      super
+
+    else
+      redirect_to new_user_session_path, notice: 'Please sign in'
+    end
   end
 end
