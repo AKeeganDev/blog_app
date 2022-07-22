5d809835c82a5af74990c9273567bf5f49b66860
Aaron Keegan <AKeegan.Dev@gmail.com>
2022-07-21 21:00:16 -0400

    fix linter

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,8 +7,6 @@ class User < ApplicationRecord
   has_many :likes
   has_many :comments
 
-  roles = %i[admin user].freeze
-
   validates :name, presence: true
   validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
